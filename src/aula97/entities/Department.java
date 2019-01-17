23355a9bcc035a5505c05d43cdf2ccb932331f37
Eric Hiroshi <erichiroshi@gmail.com>
2019-01-17 14:58:30 -0300

    Acrescentado aula 97

---
+++ src/aula97/entities/Department.java
@@ -0,0 +1,24 @@
+package aula97.entities;
+
+public class Department {
+
+	private String name;
+
+	public Department() {
+
+	}
+
+	public Department(String name) {
+		this.name = name;
+	}
+
+	// métodos getters and setters
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
