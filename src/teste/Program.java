cb2b70f1ce90ebb795beee93a66e543573e6c700
Eric Hiroshi <erichiroshi@gmail.com>
2019-02-25 10:31:58 -0400

    add aula 188

--- src/teste/Program.java
+++ src/teste/Program.java
@@ -3,35 +3,41 @@ package teste;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		List<Product> list = new ArrayList<>();
+		String path = "C:\\Users\\erich\\OneDrive\\Área de Trabalho\\Udemy\\Java curso completo\\arquivos aulas\\in aula 189.txt";
+		Map<String, Integer> votos = new LinkedHashMap<>();
 
-		String pathFile = "C:\\Users\\erich\\OneDrive\\Área de Trabalho\\Udemy\\Java curso completo\\arquivos aulas\\in aula 179.txt";
-
-		try (BufferedReader br = new BufferedReader(new FileReader(pathFile))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
 			String line = br.readLine();
 
 			while (line != null) {
 
 				String[] fields = line.split(",");
-				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
-				line = br.readLine();
+				String nome = fields[0];
+				Integer voto = Integer.parseInt(fields[1]);
 
+				if (votos.containsKey(nome))
+					voto += votos.get(nome);
+
+				votos.put(nome, voto);
+				line = br.readLine();
+			}
+
+			for (String c : votos.keySet()) {
+
+				System.out.println(c + ": " + votos.get(c));
 			}
 
 		} catch (IOException e) {
 			System.out.println("Error: " + e.getMessage());
 		}
-
-		System.out.println("Most expensive:");
-		System.out.println(CalculationService.max(list));
 
 	}
 }
